2f731c1b54ca40534988b42136b08cdb939dba32
nhanpro <nhunguyenthanhnhu83@gmail.com>
2026-05-15 17:26:46 +0700

    craate 2 file  repository sport insert db and setup exeption for service

---
+++ BookingandInventory/src/main/java/com/Gostay/BookingandInventory/exeption/ErrorCode.java
@@ -0,0 +1,11 @@
+package com.Gostay.BookingandInventory.exeption;
+
+import org.springframework.http.HttpStatusCode;
+
+public interface ErrorCode {
+    boolean isSuccess();
+    int getStatus();
+    String getCode();
+    String getMessage();
+    HttpStatusCode getHttpStatus();
+}
