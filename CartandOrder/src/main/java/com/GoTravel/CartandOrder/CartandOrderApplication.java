cd9286b50b35eb5af20971feb54ec445c8ec72bb
nhanpro <nhunguyenthanhnhu83@gmail.com>
2026-05-16 18:11:44 +0700

    create project CartandOrder

---
+++ CartandOrder/src/main/java/com/GoTravel/CartandOrder/CartandOrderApplication.java
@@ -0,0 +1,13 @@
+package com.GoTravel.CartandOrder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CartandOrderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CartandOrderApplication.class, args);
+	}
+
+}
