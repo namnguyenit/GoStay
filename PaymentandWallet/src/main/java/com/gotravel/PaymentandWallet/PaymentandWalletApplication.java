9f3a83557494746e176155265320ec791ec552e7
nhanpro <nhunguyenthanhnhu83@gmail.com>
2026-05-18 00:30:09 +0700

    create project PaymentandWallet'

---
+++ PaymentandWallet/src/main/java/com/gotravel/PaymentandWallet/PaymentandWalletApplication.java
@@ -0,0 +1,13 @@
+package com.gotravel.PaymentandWallet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentandWalletApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaymentandWalletApplication.class, args);
+	}
+
+}
