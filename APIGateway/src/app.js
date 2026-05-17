422a98ed41e892e56706c148dbfd173784324a93
namnguyenit <97675663+namnguyenit@users.noreply.github.com>
2026-05-17 13:24:13 +0700

    update checkhealth

--- APIGateway/src/app.js
+++ APIGateway/src/app.js
@@ -8,9 +8,14 @@ app.use(cors());
 
 setupProxy(app);
 
-app.get('/health', (req, res) =>{
-    res.json({
-        message: 'API gateway đang hoạt động'
+app.get('/health', (req, res) => {
+    // Log ra console để ghi nhận có request (tạo activity log)
+    console.log(`[${new Date().toISOString()}] Health check pinged!`);
+    res.status(200).json({
+        status: 'OK',
+        message: 'API gateway đang hoạt động bình thường',
+        uptime: process.uptime(),
+        timestamp: new Date().toISOString()
     });
 });
 
